a679cb031eb00ddd7edb22c3dfbd1888a01eac86
Brayhan De Aza <brayhandeaza@gmail.com>
2021-01-26 11:25:51 -0500

    heroku db added

--- db.js
+++ db.js
@@ -1,11 +1,20 @@
 const { Sequelize } = require('sequelize')
 
-const db = new Sequelize({
-    dialect: "postgres",
-    database: "chatsapp",
-    username: "postgres",
-    password: "Alicia01",
-})
+let db;
+const production = true
+
+if (production) {
+    db = new Sequelize("postgres://qjsyxwqtmzijgv:5c603533f689405a248ff26f6a9375c55458f9304c9e8823d106327c1484aa30@ec2-52-200-119-0.compute-1.amazonaws.com:5432/d8b0a4lrcno9pc")
+
+} else {
+    db = new Sequelize({
+        dialect: "postgres",
+        database: "chatsapp",
+        username: "postgres",
+        password: "Alicia01",
+    })
+}
+
 
 const connection = () => {
     try {
