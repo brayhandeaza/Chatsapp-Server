341294728629c08199a94e48c3efe7c722c3d66a
Brayhan De Aza <brayhandeaza@gmail.com>
2021-01-26 11:31:03 -0500

    heroku db added

--- app.js
+++ app.js
@@ -1,35 +1,21 @@
 const express = require("express")
 const app = express()
-
 const http = require("http").createServer(app)
 const Server = require("socket.io")
 const io = Server(http)
-
 const PORT = process.env.PORT || 5000
 const cors = require("cors")
 const bodyParser = require('body-parser')
-const { Op } = require('sequelize')
-const Chats = require("./sockets/Conversations")
+
 
 // const WebSocket = require('ws')
 // const wss = new WebSocket.Server({ http })
 
-// socket
-// wss.on("connection", (ws) => {
-//     console.log("A new client Conneceted")
-//     ws.on("message", async (fromClient) => {
-//         const { user, conversation } = JSON.parse(fromClient)
-//         const chats = new Chats(ws) 
-//         chats.conversation(user.id)
-
-//     })
-
-//     ws.emit("test", {msg: "Hello from server"})
-// })
 
 io.on('connection', (socket) => {
     console.log('a user connected');
 });
+
 
 // check database connection
 const { connection } = require('./db')
